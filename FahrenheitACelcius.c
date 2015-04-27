50afb4fd768d08dcba725e863a89976a53e1d8b4
DanielMarcelletti <danimarche32@gmail.com>
2015-04-27 03:24:18 -0300

    Primera versión usando For

--- FahrenheitACelcius.c
+++ FahrenheitACelcius.c
@@ -1,5 +1,20 @@
-/* Escribe en pantalla una tabla con una cierta temperatura en grados celcius
- * y a su derecha dicha temperatura expresada en fahrenheit
+/* Escribe en pantalla una tabla con una cierta temperatura en grados fahrenheit
+ * y a su derecha dicha temperatura expresada en celcius
  * Daniel Marcelletti
- * 13/04/2015
+ * Daian Pato
+ * 27/04/2015
  */
+
+#include <stdio.h>
+
+#define LOWER 0 /* límite inferior de la tabla */
+#define UPPER 300 /* límite superior */
+#define STEP 20 /* tamaño del incremento */
+
+main ()
+{
+	int fahr;
+	printf("Grados\tFahrenheit\n");
+	for (fahr = LOWER ;fahr <= UPPER ;fahr = fahr + STEP)
+		printf("%3d \t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
